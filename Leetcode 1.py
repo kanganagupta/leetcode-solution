a034276c87891e6d7f607f5065fe269cd7072de5
Kangana Gupta <kanganagupta2005@gmail.com>
2026-04-13 12:19:16 +0530

    Implement twoSum function for finding indices

--- Leetcode 1.py
+++ Leetcode 1.py
@@ -1,1 +1,14 @@
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+
+        for ind,val in enumerate(nums):
+            dic[val] = ind
+
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in nums and dic[x] != i:
+                return i, dic[x]
+        
+       
